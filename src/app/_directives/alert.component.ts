49eb5a0886e876ab4d7028607cb3187ff765d248
a190884810 <39787887+a190884810@users.noreply.github.com>
2019-02-16 00:17:18 -0600

    Add files via upload

---
+++ src/app/_directives/alert.component.ts
@@ -0,0 +1,28 @@
+﻿import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+// For observable function
+import { AlertService } from '../_services';
+
+@Component({
+    selector: 'alert',
+    templateUrl: 'alert.component.html'
+})
+// OnInit: 一个生命周期钩子，它会在 Angular 初始化完了该指令的所有数据绑定属性之后调用。 定义 ngOnInit() 方法可以处理所有附加的初始化任务
+// OnDestroy: 一个生命周期钩子，它会在指令、管道或服务被销毁时调用。 用于在实例被销毁时，执行一些自定义清理代码。
+// export: export 语句用于从文件（或模块）中导出函数， 对象或者基础类型， 语法如下
+export class AlertComponent implements OnInit , OnDestroy {
+    private subscription: Subscription;
+    message: any; // any: 在任意值上访问任何属性都是允许的：
+
+    constructor(private alertService: AlertService) { }
+
+    ngOnInit() {
+        this.subscription = this.alertService.getMessage().subscribe(message => {  // 描述(subscribe)反正就是直接抓取对象的值
+            this.message = message; 
+        });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,48 @@
+﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {HeaderComponent} from "./header/header.component";
-import { HomeComponent } from './home/home.component';
-import { SearchRecipeComponent } from './search-recipe/search-recipe.component';
-import {HttpClientModule} from "@angular/common/http";
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+
+import { AlertComponent } from './_directives';
+import { AuthGuard } from './_guards';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AlertService, AuthenticationService, UserService } from './_services';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    SearchRecipeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    providers: [
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService,
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
